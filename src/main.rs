ffa88236efe500891ec7dd01b58fb7597cee4647
RutaTang <rutatangpersonal@gmail.com>
2022-08-02 21:27:34 +0800

    enable menu info scroll

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,12 @@ use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout},
     style::{self, Color, Modifier, Style},
-    widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
-    Frame, Terminal, text::Text,
+    text::Text,
+    widgets::{Block, Borders, List, ListItem, ListState, Paragraph, Wrap},
+    Frame, Terminal,
 };
 
-//Todo: test long text for menu info and then support scrolling
+//todo: draw circle of fifth graph
 fn ui<B: Backend>(f: &mut Frame<B>, menu_state: &mut MenuState, board_state: &mut BoardState) {
     let dashboard = Layout::default()
         .direction(Direction::Horizontal)
@@ -43,8 +44,12 @@ fn ui<B: Backend>(f: &mut Frame<B>, menu_state: &mut MenuState, board_state: &mu
         Board::MenuInfo => menuinfoboard.border_style(Style::default().fg(Color::Yellow)),
         _ => menuinfoboard,
     };
-    let menuinfo_text = Text::raw(menu_state.infos.get(menu_state.state.selected().unwrap()).unwrap());
-    let menuinfo_text = Paragraph::new(menuinfo_text).block(menuinfoboard);
+    let selected_idx = menu_state.state.selected().unwrap();
+    let menuinfo_text = Text::raw(menu_state.infos.get(selected_idx).unwrap());
+    let menuinfo_text = Paragraph::new(menuinfo_text)
+        .block(menuinfoboard)
+        .scroll((menu_state.info_scrolls[selected_idx], 0))
+        .wrap(Wrap { trim: true });
     f.render_widget(menuinfo_text, menu[0]);
 
     let menuselectboard = Block::default().title("Menu Select").borders(Borders::ALL);
@@ -95,6 +100,7 @@ impl BoardState {
 struct MenuState {
     items: Vec<String>,
     infos: Vec<String>,
+    info_scrolls: Vec<u16>,
     state: ListState,
 }
 impl MenuState {
@@ -103,12 +109,14 @@ impl MenuState {
             items: vec![],
             infos: vec![],
             state: ListState::default(),
+            info_scrolls: vec![],
         }
     }
     fn set_items(&mut self, items: Vec<String>, infos: Vec<String>) {
-        assert!(items.len() == infos.len(),"shold be same length");
+        assert!(items.len() == infos.len(), "shold be same length");
         self.items = items;
         self.infos = infos;
+        self.info_scrolls = vec![0; self.infos.len()];
         self.state.select(Some(0));
     }
     fn next(&mut self) {
@@ -128,7 +136,7 @@ impl MenuState {
         let i = match self.state.selected() {
             Some(i) => {
                 if i == 0 {
-                    self.items.len()-1
+                    self.items.len() - 1
                 } else {
                     i - 1
                 }
@@ -136,6 +144,12 @@ impl MenuState {
             None => 0,
         };
         self.state.select(Some(i));
+    }
+    fn scroll_up(&mut self, idx: usize) {
+        self.info_scrolls[idx] = self.info_scrolls[idx].saturating_sub(1);
+    }
+    fn scroll_down(&mut self, idx: usize) {
+        self.info_scrolls[idx] = self.info_scrolls[idx].saturating_add(1);
     }
 }
 
@@ -166,7 +180,13 @@ fn main() -> Result<(), io::Error> {
     // ui thread
     let mut board_state = BoardState::new();
     let mut menu_state = MenuState::new();
-    menu_state.set_items(vec!["Circle of fifth".into(),"Harmonic".into()],vec!["This is a info for COF".into(),"Oh yes, a test text".into()]);
+    menu_state.set_items(
+        vec!["Circle of fifth".into(), "Harmonic".into()],
+        vec![
+            "This is a info for COF Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis. Ruta!".into(),
+            "Oh yes, a test text".into(),
+        ],
+    );
     loop {
         let input_event = rx.recv().unwrap();
         match input_event {
@@ -187,15 +207,19 @@ fn main() -> Result<(), io::Error> {
                 KeyEvent {
                     code: KeyCode::Down | KeyCode::Char('j'),
                     modifiers: KeyModifiers::NONE,
-                } => {
-                    menu_state.next();
-                }
+                } => match board_state.current_board {
+                    Board::MenuSelect => menu_state.next(),
+                    Board::MenuInfo => menu_state.scroll_down(menu_state.state.selected().unwrap()),
+                    _ => {}
+                },
                 KeyEvent {
                     code: KeyCode::Up | KeyCode::Char('k'),
                     modifiers: KeyModifiers::NONE,
-                } => {
-                    menu_state.previous();
-                }
+                } => match board_state.current_board {
+                    Board::MenuSelect => menu_state.previous(),
+                    Board::MenuInfo => menu_state.scroll_up(menu_state.state.selected().unwrap()),
+                    _ => {}
+                },
                 KeyEvent {
                     code: KeyCode::Char('u'),
                     modifiers: KeyModifiers::NONE,
